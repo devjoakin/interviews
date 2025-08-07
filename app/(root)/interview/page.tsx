bc3292a02b3a36f9a59588523852a5eb2b514669
juakidev <dev.juaki@gmail.com>
2025-08-07 19:19:53 +0200

    ui to intervews page

---
+++ app/(root)/interview/page.tsx
@@ -0,0 +1,12 @@
+import Agent from '@/components/Agent';
+
+const Page = () => {
+  return (
+    <>
+      <h3>Creación de entrevistas</h3>
+      <Agent userName='You' userId='user1' type='generate' />
+    </>
+  );
+};
+
+export default Page;
